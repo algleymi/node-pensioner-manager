cfd470bb9c18d6c6e764decc4f64742c8670e3bf
Arno Schutijzer <arno.schutijzer@skryv.com>
2022-02-21 18:53:28 +0100

    get age of package

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	registry "refsiverdur.org/node-pensioner-manager/v2/registry"
+)
+
 func main() {
-	panic("aaaa")
+	age := registry.GetPackageAge("@vleesbrood/unbg")
+	println(age)
 }
